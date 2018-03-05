8ca7e04929c9b104cfb3ce0ec224f05c2e202c5d
Isaac Tay <Isaac@makerforce.io>
2018-03-05 16:34:16 +0800

    Might work now maybe

--- src/mixer_node.py
+++ src/mixer_node.py
@@ -18,6 +18,48 @@ def controller_callback(data):
             rospy.Subscriber(datum.name, Transform, input_callback_make(datum.name))
             inputs[datum.name] = [0, datum.weight]
 
+def get_part(x, i, i2):
+    if i == 0:
+        x = x.translation
+        if i2 == 0:
+            return x.x
+        else if i2 == 1:
+            return x.y
+        else if i2 == 2:
+            return x.z
+    else if i == 1:
+        x = x.rotation
+        if i2 == 0:
+            return x.x
+        else if i2 == 1:
+            return x.y
+        else if i2 == 2:
+            return x.z
+        else if i2 == 3:
+            return x.w
+    return None
+
+def set_part(x, v, i, i2):
+    if i == 0:
+        x = x.translation
+        if i2 == 0:
+            x.x = v
+        else if i2 == 1:
+            x.y = v
+        else if i2 == 2:
+            x.z = v
+    else if i == 1:
+        x = x.rotation
+        if i2 == 0:
+            x.x = v
+        else if i2 == 1:
+            x.y = v
+        else if i2 == 2:
+            x.z = v
+        else if i2 == 3:
+            x.w = v
+    return x
+
 def main():
     pub = rospy.Publisher('mixer_out', Transform, queue_size=10)
 
@@ -27,7 +69,11 @@ def main():
     rospy.Subscriber('autoforce_offb_controller', NodeWeightArray, controller_callback)
     
     while not rospy.is_shutdown():
-        total = sum([inputs[key] for key in inputs.keys()])
-        out = sum([inputs[key]/total * inputs[key] for key in inputs.keys()])
-        pub.publish(out)
+        output = Transform()
+        idxs = [(i, i2) for i2 in range(i+3) for i in range(2)]
+        for i, i2 in idxs:
+            total = sum([get_part(input[key], i, i2) for key in inputs.keys()])
+            out = sum([get_part(input[key], i, i2)/total * get_part(inputs[key], i, i2) for key in inputs.key()])
+            output = set_part(output, out, i, i2)
+        pub.publish(output)
         rate.sleep()
